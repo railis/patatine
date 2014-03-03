8b74f7e8afb8cf574a0e84739c618bb97ab2a2ba
Dominik Sito <dominik.sito@gmail.com>
2014-03-03 01:16:22 +0100

    Finishes unifying commands

--- lib/patatine/cli/cmd.rb
+++ lib/patatine/cli/cmd.rb
@@ -1,16 +1,12 @@
 module Patatine
   module CLI
     class Cmd < Thor
-      desc "deploy APP ENV", "runs command on opsworks"
+      %w{deploy rollback restart update_custom_cookbooks}.each do |command|
+        desc "#{command} APP ENV", "runs '#{command}' on matched instances"
 
-      def deploy(app, env)
-        Opsworks::Command.new(app, env, "deploy").run!
-      end
-
-      desc "update_custom_cookbooks APP ENV", "runs command on opsworks"
-
-      def update_custom_cookbooks(app, env)
-        Opsworks::Command.new(app, env, "update_custom_cookbooks").run!
+        define_method command do |app, env|
+          Opsworks::Command.new(app, env, command).run!
+        end
       end
     end
   end
